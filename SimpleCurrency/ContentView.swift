a8471901bc3b085658da671302b383cb3d58aa6d
Everton Carneiro <everton.mercante@gmail.com>
2021-05-22 07:53:51 -0300

    Initial Commit

---
+++ SimpleCurrency/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleCurrency
+//
+//  Created by Everton Carneiro on 22/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleCurrency/SimpleCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCurrencyApp.swift
+//  SimpleCurrency
+//
+//  Created by Everton Carneiro on 22/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
