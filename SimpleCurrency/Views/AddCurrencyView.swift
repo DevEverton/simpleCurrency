53f7ddf8ac92a5189f844fa41d9c410711d6b031
Everton Carneiro <everton.mercante@gmail.com>
2021-06-26 10:57:31 -0300

    Implemented edit mode on selected currencies

--- SimpleCurrency/Views/AddCurrencyView.swift
+++ SimpleCurrency/Views/AddCurrencyView.swift
@@ -14,47 +14,53 @@ struct AddCurrencyView: View {
     @State var filteredList = [Country]()
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Edit")
-                .font(.system(size: 30, weight: .semibold, design: .rounded))
-                .padding(.leading, 10)
-                .padding(.top)
-            SearchBar(searchText: $searchText, listType: .addCountry, countryListVM: countryListVM, filteredList: $filteredList)
-                .padding(.vertical, 16)
-                .padding(.horizontal, 10)
-            List {
-                Section(header: Text("Selected Currencies")) {
-                    ForEach(countryListVM.savedCountries) { country in
-                        CurrencyCell(country: country)
-                            
-                    }
-                    .onDelete { indexSet in
-                        countryListVM.addBackToAllCountries(countryListVM.savedCountries[indexSet.first!])
-                        countryListVM.sortAddCountryList()
-                        countryListVM.savedCountries.remove(atOffsets: indexSet)
-                    }
-                    .animation(.linear(duration: 0.3))
-                    .onReceive(countryListVM.$addCountryList, perform: { _ in
-                        filteredList = countryListVM.addCountryList.sorted(by: { $0.name < $1.name })
+        NavigationView {
+            VStack(alignment: .leading) {
 
-                    })
+                SearchBar(searchText: $searchText, listType: .addCountry, countryListVM: countryListVM, filteredList: $filteredList)
+                    .padding(.vertical, 16)
+                    .padding(.horizontal, 10)
+                List {
+                    Section(header: Text("Selected Currencies")) {
+                        ForEach(countryListVM.savedCountries) { country in
+                            CurrencyCell(country: country)
+                                
+                        }
+                        .onDelete { indexSet in
+                            countryListVM.addBackToAllCountries(countryListVM.savedCountries[indexSet.first!])
+                            countryListVM.sortAddCountryList()
+                            countryListVM.savedCountries.remove(atOffsets: indexSet)
+                        }
+                        .onMove(perform: { indices, newOffset in
+                            countryListVM.savedCountries.move(fromOffsets: indices, toOffset: newOffset)
+                        })
+                        .onReceive(countryListVM.$addCountryList, perform: { _ in
+                            filteredList = countryListVM.addCountryList.sorted(by: { $0.name < $1.name })
+
+                        })
+                    }
+                    
+                    Section(header: Text("All Currencies")) {
+                        ForEach(filteredList) { country in
+                            CurrencyCell(country: country)
+                                .onTapGesture {
+                                    countryListVM.addCountry(country: country)
+                                    countryListVM.remove(country: country)
+                                }
+                        }
+                        .animation(.easeOut(duration: 0.5))
+
+
+                    }
+
                 }
-                
-                Section(header: Text("All Currencies")) {
-                    ForEach(filteredList) { country in
-                        CurrencyCell(country: country)
-                            .onTapGesture {
-                                countryListVM.addCountry(country: country)
-                                countryListVM.remove(country: country)
-                            }
-                    }
-                    .animation(.easeOut(duration: 0.5))
-
+                .listStyle(InsetGroupedListStyle())
+                .navigationTitle("Select")
+                .toolbar {
+                    EditButton()
                 }
 
             }
-            .listStyle(InsetGroupedListStyle())
-
         }
 
     }
--- SimpleCurrency/Views/ChooseBaseCurrencyView.swift
+++ SimpleCurrency/Views/ChooseBaseCurrencyView.swift
@@ -18,43 +18,46 @@ struct ChooseBaseCurrencyView: View {
 
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("All Currencies")
-                .font(.system(size: 30, weight: .semibold, design: .rounded))
-                .padding(.leading, 10)
-                .padding(.top)
-            
-            SearchBar(searchText: $searchText, listType: .allCountries, countryListVM: countryListVM, filteredList: $filteredList)
-                .padding(.vertical, 16)
-                .padding(.horizontal, 10)
-            List {
-                ForEach(filteredList) { country in
-                    HStack {
-                        CurrencyCell(country: country)
-                            .onTapGesture {
-                                countryListVM.baseCountry = country
-                                countryListVM.getCurrencyList(from: countryListVM.baseCountry.currency.code)
-                                countryListVM.multiplier = 0.0
-                                presentationMode.wrappedValue.dismiss()
+        NavigationView {
+            VStack(alignment: .leading) {
+    //            Text("All Currencies")
+    //                .font(.system(size: 30, weight: .semibold, design: .rounded))
+    //                .padding(.leading, 10)
+    //                .padding(.top)
+                
+                SearchBar(searchText: $searchText, listType: .allCountries, countryListVM: countryListVM, filteredList: $filteredList)
+                    .padding(.vertical, 16)
+                    .padding(.horizontal, 10)
+                List {
+                    ForEach(filteredList) { country in
+                        HStack {
+                            CurrencyCell(country: country)
+                                .onTapGesture {
+                                    countryListVM.baseCountry = country
+                                    countryListVM.getCurrencyList(from: countryListVM.baseCountry.currency.code)
+                                    countryListVM.multiplier = 0.0
+                                    presentationMode.wrappedValue.dismiss()
+                                }
+                            Spacer()
+                            if countryListVM.baseCountry.name == country.name {
+                                Image(systemName: "checkmark.circle.fill")
+                                    .font(.system(size: 16, weight: .regular))
+                                    .foregroundColor(Color.green)
                             }
-                        Spacer()
-                        if countryListVM.baseCountry.name == country.name {
-                            Image(systemName: "checkmark.circle.fill")
-                                .font(.system(size: 16, weight: .regular))
-                                .foregroundColor(Color.green)
                         }
+                        
                     }
-                    
+                    .animation(.easeOut(duration: 0.5))
+                    .onReceive(countryListVM.$allCountries, perform: { _ in
+                        filteredList = countryListVM.allCountries.sorted(by: { $0.name < $1.name })
+
+                    })
                 }
-                .animation(.easeOut(duration: 0.5))
-                .onReceive(countryListVM.$allCountries, perform: { _ in
-                    filteredList = countryListVM.allCountries.sorted(by: { $0.name < $1.name })
+                .listStyle(InsetGroupedListStyle())
+                .navigationTitle("All currencies")
+     
 
-                })
             }
-            .listStyle(InsetGroupedListStyle())
- 
-
         }
 
         
